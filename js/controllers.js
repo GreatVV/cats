ea609dac0765f77c9c3cea50e4624c8b4ba6541a
GreatVV <marinegreatvv@yahoo.com>
2013-08-30 02:32:46 +0400

    Radiobutton works okay Change cat picture on dog picture

--- js/controllers.js
+++ js/controllers.js
@@ -4,24 +4,38 @@
 
 var controllers = angular.module('catsApp.controllers', []).controller('MainController',  ['$scope', 'backendService', function($scope, backendService) {
 	$scope.animals = [
-	{value:"котика"},
-	{value:"щенка"}];
+	{"value":"котика", "id" : "animal1"},
+	{"value":"щенка", "id" : "animal2"}];
 	
 	$scope.actions = [
-	{value:"взять"},
-	{value:"помочь"}];
+	{"value":"взять", "id" : "action1"},
+	{"value":"помочь", "id" : "action2"}];
+
+	$scope.animal = "animal1";
+	$scope.action = "action1";
 
 	var body = angular.element(document.querySelector('body'));
 	
-	 $scope.change = function() {
-		if ($scope.animal == "котиков") {
-			$scope.animal = "щенков";
+	 $scope.changeAnimal = function() {
+		if ($scope.animal != "animal1") {
+			console.log("animal: "+$scope.animal)
 			body.css("backgroundImage",'url(http://zoo-farm.ru/wp-content/uploads/2011/09/kak-pravilno-vybrat-shhenka-samoedskoj-lajki.jpg)');
 		} else {
-			$scope.animal = "котиков";
+			console.log("!!!animal: "+$scope.animal)
 			body.css("backgroundImage",'url(http://img7.joyreactor.cc/pics/post/full/anon-%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%BD%D0%B0%D1%80%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-648977.jpeg)');
 		}
 	} 
+
+	$scope.changeWordForm = function() {
+		if ($scope.action == "action2") {
+			$scope.animals[0].value = "котикам";
+			$scope.animals[1].value = "щенкам";
+		} else {
+			$scope.animals[0].value = "котика";
+			$scope.animals[1].value = "щенка";
+		}
+
+	}
   }])
 .controller('DescController',['$scope', 'backendService', function($scope,backendService) {
 	$scope.images = backendService.animals[0].images;
--- js/services.js
+++ js/services.js
@@ -16,15 +16,83 @@ service("backendService",['$window',function($window) {
 	}
 	this.animals = [
 		new Animal("Мурзик","http://vk.com",[
-		{photo:"http://cs425220.vk.me/v425220115/50b0/t-FH6JY79u8.jpg"},
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
 		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
 		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
 	]),
 		new Animal("Барсик","http://vk.com",[
-		{photo:"http://cs425220.vk.me/v425220115/50b0/t-FH6JY79u8.jpg"},
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
 		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
 		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
-	])
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),new Animal("Барсик","http://vk.com",[
+		{photo:"http://cs309122.vk.me/v309122604/8808/2-Ry9BmLYmM.jpg"},
+		{photo:"http://cs425220.vk.me/v425220115/50b8/8ejxrSiVNo8.jpg"},
+		{photo:"http://cs424429.vk.me/v424429115/1f42/o1bR_uFO36w.jpg"},
+	]),
 	];
 }
 	])
