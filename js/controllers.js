0288862f6f9cbd1d4f6ac64fe2e7487bd6341d09
GreatVV <marinegreatvv@yahoo.com>
2013-09-04 07:23:30 +0700

    Add couple animations

--- js/controllers.js
+++ js/controllers.js
@@ -4,30 +4,30 @@
 
 var controllers = angular.module('catsApp.controllers', []).controller('MainController',  ['$scope', 'backendService', function($scope, backendService) {
 	$scope.animals = [
-	{"value":"котика", "id" : "animal1"},
-	{"value":"щенка", "id" : "animal2"}];
+	{"value":"котика", "id" : "cat"},
+	{"value":"щенка", "id" : "dog"}];
 	
 	$scope.actions = [
-	{"value":"взять", "id" : "action1"},
-	{"value":"помочь", "id" : "action2"}];
+	{"value":"взять", "id" : "take"},
+	{"value":"помочь", "id" : "gift"}];
 
-	$scope.animal = "animal1";
-	$scope.action = "action1";
+	$scope.animal = "dog";
+	$scope.action = "take";
 
 	var body = angular.element(document.querySelector('body'));
 	
 	 $scope.changeAnimal = function() {
-		if ($scope.animal != "animal1") {
-			console.log("animal: "+$scope.animal)
+	 	console.log("animal: "+$scope.animal)
+
+		if ($scope.animal != "dog") {
 			body.css("backgroundImage",'url(http://zoo-farm.ru/wp-content/uploads/2011/09/kak-pravilno-vybrat-shhenka-samoedskoj-lajki.jpg)');
 		} else {
-			console.log("!!!animal: "+$scope.animal)
 			body.css("backgroundImage",'url(http://img7.joyreactor.cc/pics/post/full/anon-%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%BD%D0%B0%D1%80%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-648977.jpeg)');
 		}
 	} 
 
 	$scope.changeWordForm = function() {
-		if ($scope.action == "action2") {
+		if ($scope.action == "gift") {
 			$scope.animals[0].value = "котикам";
 			$scope.animals[1].value = "щенкам";
 		} else {
